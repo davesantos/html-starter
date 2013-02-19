2142df071166cf4900492bce6c73129ec34bd528
Dave Santos <info@davesantos.com>
2013-02-18 21:12:09 -0800

    Updated config to production, updated normalize

--- config/compass.rb
+++ config/compass.rb
@@ -9,6 +9,8 @@ sass_dir = "sass"
 images_dir = "assets"
 javascripts_dir = "js"
 
+output_style = :expanded
+environment = :production
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
