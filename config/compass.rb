bf2a0734341be4fba27eea0a5371ce20b2d5c4a9
Dave Santos <info@davesantos.com>
2014-06-03 21:32:19 -0700

    Moved mixins

--- config/compass.rb
+++ config/compass.rb
@@ -5,7 +5,7 @@ require "susy"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "assets"
 javascripts_dir = "js"
 
