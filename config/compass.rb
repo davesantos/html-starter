36ed3cb4684ae906d6b04ce941769864eaac4574
Dave Santos <info@davesantos.com>
2014-10-01 13:01:28 -0700

    Moved scss folder

--- config/compass.rb
+++ config/compass.rb
@@ -5,7 +5,7 @@ require "susy"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
+sass_dir = "css/scss"
 images_dir = "assets"
 javascripts_dir = "js"
 
