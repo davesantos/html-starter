27bef90b2b2be67c56d312913e7d6196c944b668
Dave Santos <info@davesantos.com>
2014-11-03 16:46:32 -0800

    Moved files around

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "src/css"
-sass_dir = "src/_sass"
-images_dir = "src/assets"
-javascripts_dir = "src/js"
+css_dir = "css"
+sass_dir = "_sass"
+images_dir = "assets"
+javascripts_dir = "js"
 
 output_style = :expanded
 environment = :production
