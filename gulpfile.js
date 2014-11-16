ace5aaf6522896c471553c52fc911c3de917dcbe
Dave Santos <info@davesantos.com>
2014-11-15 23:37:53 -0800

    Added change and util package

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp'),
-	jade = require('gulp-jade'),
+	changed = require('gulp-changed'),
 	compass = require('gulp-compass'),
+	gutil = require('gulp-util'),
+	jade = require('gulp-jade'),
+	livereload = require('gulp-livereload'),
 	newer = require('gulp-newer'),
-	prettify = require('gulp-prettify'),
-	livereload = require('gulp-livereload');
-
+	prettify = require('gulp-prettify');
 
 function errorHandler(error) {
 	console.error(String(error));
 	this.emit('end');
+	gutil.beep();
 }
 
 // Compass compile and livereload
 gulp.task('compass', function(){
 	return gulp.src('./_sass/*')
+		.pipe(changed('css', {extension: '.css'}))
 		.pipe(compass({
 			config_file: 'config.rb',
 			css: './css',
@@ -26,11 +29,13 @@ gulp.task('compass', function(){
 
 gulp.task('jade', function(){
 	gulp.src('./*.jade')
+		.pipe(changed('.', {extension: '.html'}))
 		.pipe(jade({
 			pretty: true
 		}))
 		.pipe(gulp.dest('.'))
 		.pipe(livereload());
+		gutil.log(gutil.colors.green('jade reloaded'));
 })
 
 gulp.task('indent', function(){
@@ -49,8 +54,8 @@ gulp.task('indent', function(){
 gulp.task('watch', function(){
 	var server = livereload();
 	gulp.watch('./_sass/**/*', ['compass']);
-	gulp.watch('./css/*').on('change', livereload.changed);
 	gulp.watch('./*.jade', ['jade']);
+	gulp.watch(['./js/*']).on('change', livereload.changed );
 })
 
 gulp.task('default', ['compass', 'watch']);
