776f158998b289cddd9c60f3478f17e30923dd9c
Dave Santos <info@davesantos.com>
2016-05-31 20:54:20 -0700

    Remove font

--- js/all.js
+++ js/all.js
@@ -1,17 +1,3 @@
-// WebFontConfig = {
-//   custom: {
-//     families: ['atc-duel']
-//     // urls: ['../css/all.css']
-//   }
-// };
-
-// (function(d) {
-//    var wf = d.createElement('script'), s = d.scripts[0];
-//    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
-//    s.parentNode.insertBefore(wf, s);
-// })(document);
-
-
 jQuery(function($){
 
   (function ( app, undefined ) {
