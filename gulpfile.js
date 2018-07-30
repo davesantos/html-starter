e47481748bdcb719f6b4afc8f6b8db7b1377680d
davesantos <info@davesantos.com>
2018-07-29 17:54:40 -0700

    Conver tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,9 @@ var paths = {
 };
 
 function errorHandler(error) {
-	console.error(String(error));
-	this.emit('end');
-	browserSync.notify('Error');
+  console.error(String(error));
+  this.emit('end');
+  browserSync.notify('Error');
 }
 
 gulp.task('travis', gulp.series(gulp.parallel('pug', 'indent', 'serve'), function() {
@@ -22,53 +22,53 @@ gulp.task('travis', gulp.series(gulp.parallel('pug', 'indent', 'serve'), functio
 }));
 
 gulp.task('sass', function(){
-	return gulp.src(paths.sass + '/**/*.{sass,scss}')
-		.pipe(sass().on('error', errorHandler))
-		.pipe( cleanCSS({
-		  debug: true,
-		  keepBreaks: true,
-		  keepSpecialComments: false
-		}, function(details) {
-		  console.log(details.name + ': ' + details.stats.originalSize + ' -> ' + details.stats.minifiedSize);
-		}) )
-		.pipe(gulp.dest(paths.css))
-		.pipe(browserSync.reload({stream:true}))
+  return gulp.src(paths.sass + '/**/*.{sass,scss}')
+    .pipe(sass().on('error', errorHandler))
+    .pipe( cleanCSS({
+      debug: true,
+      keepBreaks: true,
+      keepSpecialComments: false
+    }, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize + ' -> ' + details.stats.minifiedSize);
+    }) )
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('pug', function(){
-	return gulp.src('./*.pug')
-		.pipe(changed('.', {extension: '.html'}))
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest('.'));
+  return gulp.src('./*.pug')
+    .pipe(changed('.', {extension: '.html'}))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('.'));
 })
 
 gulp.task('indent', function(){
 
-	return gulp.src('*.html')
-		.pipe(prettify({
-			indent_inner_html: true,
-			indent_with_tabs: true,
-			indent_char:"	",
-			indent_size: 1,
-			preserve_newlines: false
-		}))
-		.pipe(gulp.dest('.'));
+  return gulp.src('*.html')
+    .pipe(prettify({
+      indent_inner_html: true,
+      indent_with_tabs: true,
+      indent_char:" ",
+      indent_size: 1,
+      preserve_newlines: false
+    }))
+    .pipe(gulp.dest('.'));
 
 });
 
 gulp.task('serve', gulp.series('sass', function() {
 
   browserSync.init({
-	  server: {
+    server: {
       baseDir: "./"
     }
   });
 
-	gulp.watch(paths.sass + '/**/*.{sass,scss}', gulp.parallel('sass')).on('change', browserSync.reload );
-	gulp.watch('./*.pug', gulp.parallel('pug')).on('change', browserSync.reload );
-	gulp.watch(['./js/*']).on('change', browserSync.reload );
+  gulp.watch(paths.sass + '/**/*.{sass,scss}', gulp.parallel('sass')).on('change', browserSync.reload );
+  gulp.watch('./*.pug', gulp.parallel('pug')).on('change', browserSync.reload );
+  gulp.watch(['./js/*']).on('change', browserSync.reload );
 
 }));
 
