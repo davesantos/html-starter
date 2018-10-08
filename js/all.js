e121d6b311fe3d525a8b61af84aafbd2a02fb895
davesantos <info@davesantos.com>
2018-10-08 01:27:16 -0700

    Update index

--- js/all.js
+++ js/all.js
@@ -1,16 +1,1 @@
-jQuery(function($){
-
-  (function ( app, undefined ) {
-
-    var theApp = {
-      init : function() {
-
-      }
-    }
-    $.extend(app, theApp);
-
-  })( window.MyApp = window.MyApp || {} );
-
-  MyApp.init();
-
-})
+console.log('This is working!');
