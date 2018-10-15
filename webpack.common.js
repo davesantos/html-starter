0502969f6257b95190fe475cd2e5ade3598cc411
davesantos <info@davesantos.com>
2018-10-14 19:47:37 -0700

    Add common, webpack-merge, dev

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports =  {
+
+  context: path.join(__dirname, 'src'),
+  mode: 'none',
+  entry: './main.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Starter',
+      minify: {
+        collapseWhitespace: true,
+        preserveLineBreaks: true
+      },
+      hash: true,
+      template: './index.pug'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash:5].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              // publicPath: '../'
+            }
+          },
+          "css-loader",
+          "sass-loader"
+       ]
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'html-loader',
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = Merge(CommonConfig, {
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash:5].js'
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, 'js')
+    stats: 'errors-only'
+  }
+});
