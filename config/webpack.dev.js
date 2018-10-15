2b299ab977f38c7c0ee4f12310f2cab209b42bab
davesantos <info@davesantos.com>
2018-10-15 14:03:27 -0700

    Remove mode

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,6 @@ const CommonConfig = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = Merge(CommonConfig, {
-  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash:5].js'
